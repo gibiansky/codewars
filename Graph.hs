0cf0cfcde2f38007e5f90210bc0ba9c39e5457f3
Jack Ma <jackma@wl-214-124.wpa.claremont.edu>
2014-02-01 14:12:36 -0800

    Added pathToDestination

--- Graph.hs
+++ Graph.hs
@@ -1,12 +1,14 @@
 module Graph where
 
 import Data.Array
+import Data.Graph.AStar
+import qualified Data.Set as Set
 
 import Types
 import Control.Lens
 
-neighbors :: Map -> Tile -> [Tile]
-neighbors gameMap tile = neighborTiles
+neighbors :: Map -> Tile -> Set.Set Tile
+neighbors gameMap tile = Set.fromList neighborTiles
   where
     (ix, iy) = (tile ^. tileLoc . x, tile ^. tileLoc . y)
     indices = filter isValidIndex 
@@ -29,3 +31,10 @@ neighbors gameMap tile = neighborTiles
     tileArray = array ((0, 0), (mapWidth -1, mapHeight -1)) arrayAssocs
     arrayAssocs = map toAssoc $ gameMap ^. tiles
     toAssoc tile = ((tile ^. tileLoc . x, tile ^. tileLoc . y), tile)
+
+pathToDestination :: Map -> Tile -> Tile -> Maybe [Tile]
+pathToDestination gameMap startTile endTile = aStar (neighbors gameMap) distanceFun heuristicFun goalFun startTile
+  where
+    distanceFun = (\x y -> 1)
+    heuristicFun = (\tile -> (tile ^. tileLoc . x - endTile ^. tileLoc . x) +  (tile ^. tileLoc . y - endTile ^. tileLoc . y))
+    goalFun = (== endTile)
